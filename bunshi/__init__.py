66ad271decab00c937f329fac259c1631c9b9fcd
Johannes H.Gjeraker <59526817+jhgjeraker@users.noreply.github.com>
2022-07-07 10:57:50 +0200

    Bumped to version 0.1.1

--- bunshi/__init__.py
+++ bunshi/__init__.py
@@ -1,7 +1,7 @@
 # This file is part of bunshi, a kanji breakdown cli.
 # License: GNU GPL version 3, see the file "LICENCE" for details.
 
-__VERSION__ = '0.1.0'
+__VERSION__ = '0.1.1'
 
 BREAKDOWN_PATH = 'bunshi/assets/breakdown.tsv'
 
